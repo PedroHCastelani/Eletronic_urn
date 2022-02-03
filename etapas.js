09564821ae34219b93c1c3dd33cc9f96484e378b
Pedro H. Castelani <pedro.castelani@hotmail.com>
2022-02-03 11:18:54 -0300

    Adicionando arquivos iniciais no projeto

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VERADOR', 
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                name: 'Fulando de Tal',
+                partido: 'AVC',
+                fotos: [
+                    {url: '38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                name: 'Beltrano de Tal',
+                partido: 'ASD',
+                fotos: [
+                    {url: '77222.jpg', legenda: 'Vereador'}   
+                ]        
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidados: [
+            {
+                numero: '99',
+                name: 'Ciclano',
+                partido: 'AVC',
+                vice: "Cic",
+                fotos: [
+                    {url: '99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', samll: true}
+                ]
+            },
+            {
+                numero: '84',
+                name: 'Zulano',
+                partido: 'DSA',
+                vice: "Juca",
+                fotos: [
+                    {url: '84.jpg', legenda: 'Prefeito'},
+                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', samll: true}
+                ]
+            },
+        ]
+    }
+];
---
+++ script.js
@@ -0,0 +1,89 @@
+//Interface control
+let seuVotoPara = document.querySelector('.d-1--1 span');
+let cargo = document.querySelector('.d-1--2 span');
+let desc = document.querySelector('.d-1--4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1--right');
+let numeros = document.querySelector(".d-1--3");
+
+
+//Variable environment control
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = ''; 
+
+    for(let i=0;i<etapa.numeros;i++) {    
+        if(i === 0) {
+            numeroHtml += '<div class="numero pisca"></div>';   
+        } else {  
+         numeroHtml += '<div class="numero"></div>';
+        } 
+    }
+
+    seuVotoPara.style.display = "none";
+    cargo.innerHTML = etapa.titulo;
+    desc.innerHTML = '';
+    aviso.style.display = "none";
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if (item.numero === numero) {
+            return true;
+        } else{
+            return false; 
+        }
+    });
+
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = "block";
+        aviso.style.display = "block";
+        desc.innerHTML = `Name: ${candidato.name}<br/>Partido: ${candidato.partido}`;
+
+        let fotosHtml = '';
+        for(let i in candidato.fotos) {
+            fotosHtml += `<div class="d-1--image"><img src="images/${candidato.fotos[i].url}"/>${candidato.fotos[i].legenda}</div>`;
+        }
+        lateral.innerHTML = fotosHtml;
+    }   else {
+            seuVotoPara.style.display = 'block';
+            aviso.style.display ='block';
+            desc.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>'
+
+    }
+}
+
+//Functions
+function clicou(n) {
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else{
+            atualizaInterface();
+        }   
+    }    
+}
+function branco() {
+    alert("Clicou em BRANCO")
+}
+function corrige() {
+    alert("Clicou em CORRIGE")
+}
+function confirma() {
+    alert("Clicou em CONFIRMA")
+}
+
+comecarEtapa();
